f9dae6996cb4478cd90b9c6085ddf4f6f638b1a7
Gisele Macieira <giselefchaves@gmail.com>
2023-08-10 00:29:23 -0300

    construção da página de login /signin

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,18 +1,120 @@
+import axios from "axios"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { styled } from "styled-components"
+import logo from "../assets/logo-transparente-croped.png"
+import { BASE_URL } from "../constants/urls";
 
 
 export default function SignInPage(){
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    function signIn(event){
+        event.preventDefault();
+        //body: {email, password}
+        const url = `${BASE_URL}/signin`
+        const body = {
+            email,
+            password
+        }
+        axios.post(url, body)
+            .then(resp => {
+                console.log(resp.data)
+                localStorage.setItem("user", JSON.stringify(resp.data));
+                navigate('/');
+            })
+            .catch(err => {
+                alert(err.response.data)
+            })
+    }
+
     return(
         <SingInContainer>
-            <p>Pagina de login</p>
+            <img src={logo} />
+            <SignInForm onSubmit={signIn}>
+                <SignInInput required
+                type='email'
+                placeholder="E-mail"
+                value={email}
+                onChange={e => setEmail(e.target.value)}/>
+                <SignInInput required
+                type='password'
+                placeholder="Senha"
+                value={password}
+                onChange={e => setPassword(e.target.value)}/>
+                <SignInButton type='submit'>Entrar</SignInButton>
+            </SignInForm>
+            <Link to= '/signup'>
+                Primeira vez? Cadastre-se!
+            </Link>
         </SingInContainer>
     )
 }
 
 const SingInContainer = styled.section`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  box-sizing: border-box;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-left: 33px;
+    padding-right: 33px;
+    overflow-y: scroll;
+    img{
+        width: 80%;
+        padding-bottom: 55px;
+        padding-top: 0px;
+    }
+    a{
+        font-weight: 700;
+        font-size: 15px;
+        line-height: 18px;
+        color: black;
+        text-decoration: none;
+        padding: 18px;
+    }
+`
+
+const SignInForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    width: 100%;
+    border-radius: 5px;
+`
+
+const SignInInput = styled.input`
+    background-color: #e7e6e6;
+    font-size: 20px;
+    width: 100%;
+    border-radius: 5px;
+    outline: none;
+    border: 15px;
+    height: 55px;
+    padding-left: 15px;
+    :focus {
+        border: 2px solid #ffb6b6;
+        margin: 0px;
+    }
+`
+
+const SignInButton = styled.button`
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    background-color: #ffe699;
+    font-size: 20px;
+    font-weight: 600;
+    color: #767171;
+    cursor: pointer;
+    width: 104%;
+    height: 40px;
+    margin-top: 10px;
 `
